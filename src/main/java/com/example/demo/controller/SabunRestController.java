32a6d0cd062f79173813ddb2743ddbbee9d42c74
zangsc <zangsc@hanmail.net>
2024-08-13 14:16:03 +0900

    m

--- src/main/java/com/example/demo/controller/SabunRestController.java
+++ src/main/java/com/example/demo/controller/SabunRestController.java
@@ -1,16 +1,8 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.TodoItemDto;
 import com.example.demo.service.TodoItemService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import jakarta.validation.Valid;
 
 @Slf4j
 @RequestMapping("/api/v1")
